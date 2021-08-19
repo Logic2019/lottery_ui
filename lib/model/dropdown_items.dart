2fdabfe534f2e5a36dba0a90729e9d985ac8ab37
Aristocrat <crataristo4@gmail.com>
2021-08-19 21:51:02 +0000

    added drop down model for dropdown menu item

---
+++ lib/model/dropdown_items.dart
@@ -0,0 +1,15 @@
+import 'package:lottery_ui/constants.dart';
+
+class DropDownItems {
+  final itemName;
+  final itemIcon;
+
+  DropDownItems({this.itemName, this.itemIcon});
+}
+
+List<DropDownItems> dropDownItemList = [
+  DropDownItems(itemName: jackPort, itemIcon: 'assets/images/jackport.png'),
+  DropDownItems(itemName: jackPort, itemIcon: 'assets/images/jackport.png'),
+  DropDownItems(itemName: jackPort, itemIcon: 'assets/images/jackport.png'),
+  DropDownItems(itemName: jackPort, itemIcon: 'assets/images/jackport.png'),
+];
