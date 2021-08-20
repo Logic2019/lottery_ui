cf1ac02d67909b436966e4c0c2f528cb951ad72e
Aristocrat <crataristo4@gmail.com>
2021-08-20 11:18:26 +0000

    model for list

---
+++ lib/model/lottery.dart
@@ -0,0 +1,64 @@
+import 'package:lottery_ui/constants.dart';
+
+class Lottery {
+  final image;
+  final title;
+  final subtitle;
+  final price;
+  final flag;
+  final time;
+
+  Lottery(
+      {this.image,
+      this.title,
+      this.subtitle,
+      this.price,
+      this.flag,
+      this.time});
+}
+
+//dummy list from UI
+List<Lottery> lotteryList = [
+  Lottery(
+      image: 'assets/images/im.png',
+      title: superBall,
+      subtitle: jackPort,
+      price: lotteryPrice,
+      flag: 'assets/images/usa.png',
+      time: time),
+  Lottery(
+      image: 'assets/images/im.png',
+      title: superBall,
+      subtitle: jackPort,
+      price: lotteryPrice,
+      flag: 'assets/images/canada.png',
+      time: time),
+  Lottery(
+      image: 'assets/images/im.png',
+      title: superBall,
+      subtitle: jackPort,
+      price: lotteryPrice,
+      flag: 'assets/images/britain.png',
+      time: time),
+  Lottery(
+      image: 'assets/images/im.png',
+      title: superBall,
+      subtitle: jackPort,
+      price: lotteryPrice,
+      flag: 'assets/images/china.png',
+      time: time),
+  Lottery(
+      image: 'assets/images/im.png',
+      title: superBall,
+      subtitle: jackPort,
+      price: lotteryPrice,
+      flag: 'assets/images/france.png',
+      time: time),
+  Lottery(
+      image: 'assets/images/im.png',
+      title: superBall,
+      subtitle: jackPort,
+      price: lotteryPrice,
+      flag: 'assets/images/canada.png',
+      time: time),
+];
