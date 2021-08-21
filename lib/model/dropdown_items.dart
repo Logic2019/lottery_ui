e90c6160d731efcaa96a50c64841b83b6ec6d402
Aristocrat <crataristo4@gmail.com>
2021-08-21 08:57:23 +0000

    added comments

--- lib/model/dropdown_items.dart
+++ lib/model/dropdown_items.dart
@@ -1,12 +1,15 @@
 import 'package:lottery_ui/constants.dart';
 
+///This class represents the data that will be displayed in the drop down
 class DropDownItems {
   final itemName;
   final itemIcon;
 
+  //constructor to initialise object
   DropDownItems({this.itemName, this.itemIcon});
 }
 
+//list of items to populate drop down
 List<DropDownItems> dropDownItemList = [
   DropDownItems(itemName: jackPort, itemIcon: 'assets/images/jackport.png'),
   DropDownItems(itemName: jackPort, itemIcon: 'assets/images/jackport.png'),
